b65f27531d50ecc3ff22489d60751e7728a26ebe
DanielM2292 <143299403+DanielM2292@users.noreply.github.com>
2025-01-28 21:59:08 -0500

    Cambios FacturaPage

--- acueducto_frontend/src/pages/FacturacionPage.jsx
+++ acueducto_frontend/src/pages/FacturacionPage.jsx
@@ -5,6 +5,7 @@ const FacturacionPage = () => {
         <div>
             <h1>Bienvenido a la Página de Facturación</h1>
             <h2>Aquí se mostrará la información de la facturación</h2>
+            <h3>que feo esto jaja</h3>
         </div>
     );
 };
